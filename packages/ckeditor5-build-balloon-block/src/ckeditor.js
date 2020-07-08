efd9322d954b5ebb4b09386df8ffb3ffd7b2ff2a
zadam <adam.zivner@gmail.com>
2020-07-08 21:56:27 +0200

    add tab/shift-tab shortcuts for indent/outdent block

--- packages/ckeditor5-build-balloon-block/src/ckeditor.js
+++ packages/ckeditor5-build-balloon-block/src/ckeditor.js
@@ -56,6 +56,7 @@ import Enter from '@ckeditor/ckeditor5-enter/src/enter.js';
 import ShiftEnter from '@ckeditor/ckeditor5-enter/src/shiftenter.js';
 import Typing from '@ckeditor/ckeditor5-typing/src/typing.js';
 import Undo from '@ckeditor/ckeditor5-undo/src/undo.js';
+import indentBlockShortcutPlugin from './indent_block_shortcut.js';
 
 export default class BalloonEditor extends BalloonEditorBase {}
 
@@ -106,7 +107,8 @@ BalloonEditor.builtinPlugins = [
 	Mention,
 	MentionCustomization,
 	IncludeNote,
-	ReferenceLink
+	ReferenceLink,
+	indentBlockShortcutPlugin
 ];
 
 // Editor configuration.
@@ -139,7 +141,7 @@ BalloonEditor.defaultConfig = {
 	heading: {
 		options: [
 			{ model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-			{ model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+			// heading1 is not used since that should be a note's title
 			{ model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
 			{ model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
 			{ model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
---
+++ packages/ckeditor5-build-balloon-block/src/indent_block_shortcut.js
@@ -0,0 +1,22 @@
+/**
+ * https://github.com/zadam/trilium/issues/978
+ */
+export default function indentBlockShortcutPlugin(editor) {
+	editor.keystrokes.set( 'Tab', ( data, cancel ) => {
+		const command = editor.commands.get( 'indentBlock' );
+
+		if ( command.isEnabled ) {
+			command.execute();
+			cancel();
+		}
+	} );
+
+	editor.keystrokes.set( 'Shift+Tab', ( data, cancel ) => {
+		const command = editor.commands.get( 'outdentBlock' );
+
+		if ( command.isEnabled ) {
+			command.execute();
+			cancel();
+		}
+	} );
+}
