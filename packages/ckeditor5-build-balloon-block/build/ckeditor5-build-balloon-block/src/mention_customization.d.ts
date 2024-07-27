304615c22bd754c7e89bbe3e0b57bc74593d732e
Elian Doran <contact@eliandoran.me>
2024-07-27 22:55:30 +0300

    Remove built artifacts from repository

--- packages/ckeditor5-build-balloon-block/build/ckeditor5-build-balloon-block/src/mention_customization.d.ts
+++
@@ -1,4 +0,0 @@
-export default class MentionCustomization extends Plugin {
-    afterInit(): void;
-}
-import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
--- packages/ckeditor5-build-balloon-block/build/ckeditor5-file-upload/uploadfileplugin.d.ts
+++
@@ -1,6 +0,0 @@
-export default class Uploadfileplugin extends Plugin {
-    static get requires(): (typeof FileUploadEditing)[];
-    static get pluginName(): string;
-}
-import { Plugin } from "@ckeditor/ckeditor5-core";
-import FileUploadEditing from "./src/fileuploadediting";
